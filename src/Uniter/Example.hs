c0d149e7a700c6510b63f8799acb0648c4c995f6
Eric Conlon <ejconlon@gmail.com>
2021-12-28 14:03:37 -0800

    ex

--- src/Uniter/Example.hs
+++ src/Uniter/Example.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE UndecidableInstances #-}
+-- {-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Uniter.Example where
 
-import Control.Monad.Except (Except, MonadError, runExcept, ExceptT, runExceptT)
+import Control.Monad.Except (Except, MonadError, runExcept, ExceptT, runExceptT, throwError)
 import Control.Monad.State.Strict (State, MonadState (..), gets, modify', runState, StateT (runStateT))
 import Data.Foldable (foldMap')
 import Data.Hashable (Hashable)
@@ -17,8 +18,13 @@ import Overeasy.Expressions.Free (Free, pattern FreeEmbed, pattern FreePure)
 import Uniter.Exp (Exp (..), Ty, TyF (..))
 import Control.Monad.Logic (LogicT)
 import Control.Applicative (Alternative (..))
+import Data.Functor.Foldable (Base, Corecursive (..), Recursive (..))
+import Data.Void (Void)
 
 data Void1 a
+
+data Pair a b = Pair !a !b
+  deriving stock (Eq, Show)
 
 data Aligned f a b =
     AlignedVars !a !b
@@ -26,15 +32,24 @@ data Aligned f a b =
   | AlignedStructB !(f a) !b
   deriving stock (Eq, Show)
 
+alignedPair :: Pair a b -> Aligned f a b
+alignedPair (Pair a b) = AlignedVars a b
+
 newtype Embed f a = Embed { unEmbed :: f (Free f a) }
 deriving stock instance Eq (f (Free f a)) => Eq (Embed f a)
 deriving stock instance Show (f (Free f a)) => Show (Embed f a)
 
-class Alignable m g f where
-  align :: f a -> f b -> LogicT m (Aligned g a b)
+class Alignable m f where
+  align :: f a -> f b -> LogicT m (Pair a b)
 
-instance Alignable m Void1 f => Alignable m (Embed f) (Free f) where
-  align fa fb =
+class AlignableAs m g f where
+  alignAs :: f a -> f b -> LogicT m (Aligned g a b)
+
+instance Alignable m f => AlignableAs m Void1 f where
+  alignAs fa fb = fmap alignedPair (align fa fb)
+
+instance Alignable m f => AlignableAs m (Embed f) (Free f) where
+  alignAs fa fb =
     case fa of
       FreePure a ->
         case fb of
@@ -43,16 +58,16 @@ instance Alignable m Void1 f => Alignable m (Embed f) (Free f) where
       FreeEmbed ta ->
         case fb of
           FreePure b -> pure (AlignedStructB (Embed ta) b)
-          FreeEmbed tb -> align (Embed ta) (Embed tb)
+          FreeEmbed tb -> alignAs (Embed ta) (Embed tb)
 
-instance Alignable m Void1 f => Alignable m (Embed f) (Embed f) where
-  align (Embed ta) (Embed tb) = fixAlign (align ta tb) where
+instance Alignable m f => AlignableAs m (Embed f) (Embed f) where
+  alignAs (Embed ta) (Embed tb) = fixAlign (alignAs ta tb) where
     fixAlign :: LogicT m (Aligned Void1 (Free f a) (Free f b)) -> LogicT m (Aligned (Embed f) a b)
     fixAlign xs = xs >>= bindAlign
     bindAlign :: Aligned Void1 (Free f a) (Free f b) -> LogicT m (Aligned (Embed f) a b)
-    bindAlign (AlignedVars fa fb) = align fa fb
+    bindAlign (AlignedVars fa fb) = alignAs fa fb
 
-instance MonadFail m => Alignable m Void1 TyF where
+instance MonadFail m => Alignable m TyF where
   align ta tb =
     case ta of
       TyConstF ->
@@ -62,7 +77,7 @@ instance MonadFail m => Alignable m Void1 TyF where
       TyPairF fa1 fa2 ->
         case tb of
           TyConstF -> fail "pair error"
-          TyPairF fb1 fb2 -> pure (AlignedVars fa1 fb1) <|> pure (AlignedVars fa2 fb2)
+          TyPairF fb1 fb2 -> pure (Pair fa1 fb1) <|> pure (Pair fa2 fb2)
 
 exampleLinear :: Exp
 exampleLinear =
@@ -89,57 +104,82 @@ newtype Part f = Part { unPart :: Free f VarId }
 deriving newtype instance Eq (f (Free f VarId)) => Eq (Part f)
 deriving stock instance Show (f (Free f VarId)) => Show (Part f)
 
-newtype TyRel f = TyRel { unTyRel :: Map VarId (Part f) }
-deriving newtype instance Eq (f (Free f VarId)) => Eq (TyRel f)
-deriving stock instance Show (f (Free f VarId)) => Show (TyRel f)
+makePart :: (Recursive t, Base t ~ f) => t -> Part f
+makePart = Part . go where
+  go = FreeEmbed . fmap go . project
 
-newtype TyEnv f = TyEnv { unTyEnv :: Map String (Part f) }
-deriving newtype instance Eq (f (Free f VarId)) => Eq (TyEnv f)
-deriving stock instance Show (f (Free f VarId)) => Show (TyEnv f)
+newtype BoundEnv f = BoundEnv { unBoundEnv :: Map VarId (Part f) }
+deriving newtype instance Eq (f (Free f VarId)) => Eq (BoundEnv f)
+deriving stock instance Show (f (Free f VarId)) => Show (BoundEnv f)
+
+lookupWholeVar :: (Corecursive t, Base t ~ f, Traversable f) => BoundEnv f -> VarId -> Either VarId t
+lookupWholeVar b@(BoundEnv m) v =
+  case Map.lookup v m of
+    Nothing -> Left v
+    Just (Part w) -> lookupWholeFree b w
+
+lookupWholePart :: (Corecursive t, Base t ~ f, Traversable f) => BoundEnv f -> Part f -> Either VarId t
+lookupWholePart b = lookupWholeFree b . unPart
+
+lookupWholeFree :: (Corecursive t, Base t ~ f, Traversable f) => BoundEnv f -> Free f VarId -> Either VarId t
+lookupWholeFree b w =
+  case w of
+    FreePure v -> lookupWholeVar b v
+    FreeEmbed ff -> fmap embed (traverse (lookupWholeFree b) ff)
+
+newtype FreeEnv f = FreeEnv { unFreeEnv :: Map String (Part f) }
+deriving newtype instance Eq (f (Free f VarId)) => Eq (FreeEnv f)
+deriving stock instance Show (f (Free f VarId)) => Show (FreeEnv f)
 
 data St f = St
-  { stUniq :: !VarId
-  , stTyEnv :: !(TyEnv f)
-  , stTyRel :: !(TyRel f)
+  { stUnique :: !VarId
+  , stFreeEnv :: !(FreeEnv f)
+  , stBoundEnv :: !(BoundEnv f)
   }
 deriving stock instance Eq (f (Free f VarId)) => Eq (St f)
 deriving stock instance Show (f (Free f VarId)) => Show (St f)
 
-newtype M e f a = M { unM :: ExceptT e (State (St f)) a }
-  deriving newtype (Functor, Applicative, Monad, MonadState (St f), MonadError e)
+data Error e =
+    ErrorFail !String
+  | ErrorMissingFree !String
+  | ErrorEmbed !e
+  deriving stock (Eq, Show)
 
-runM :: M e f a -> St f -> (Either e a, St f)
-runM = runState . runExceptT . unM
+newtype M e f a = M { unM :: StateT (St f) (Except (Error e)) a }
+  deriving newtype (Functor, Applicative, Monad, MonadState (St f), MonadError (Error e))
+
+runM :: M e f a -> St f -> Either (Error e) (a, St f)
+runM m st = runExcept (runStateT (unM m) st)
 
 class Substitutable f x | x -> f where
-  subReplace :: TyRel f -> x -> x
-  subFreeVars :: x -> Set VarId
+  subReplace :: BoundEnv f -> x -> x
+  subBoundVars :: x -> [VarId]
 
--- instance Substitutable f (Part f) where
---   subReplace (VM s) = go where
---     go = \case
---       ValPair v w -> ValPair (go v) (go w)
---       v@(ValVar i) -> fromMaybe v (Map.lookup i s)
---       v -> v
---   subFreeVars = \case
---     ValPair v w -> Set.union (subFreeVars v) (subFreeVars w)
---     ValVar i -> Set.singleton i
---     _ -> Set.empty
+instance (Functor f, Foldable f) => Substitutable f (Part f) where
+  subReplace (BoundEnv m) = Part . go . unPart where
+    go z =
+      case z of
+        FreePure v -> maybe z unPart (Map.lookup v m)
+        FreeEmbed fx -> FreeEmbed (fmap go fx)
+  subBoundVars = go . unPart where
+    go = \case
+      FreePure v -> [v]
+      FreeEmbed fx -> foldMap go fx
 
--- instance Substitutable f (TyRel f) where
---   subReplace m@(VM s) (VM t) = VM (Map.union s (Map.map (subReplace m) t))
---   subFreeVars = foldMap' subFreeVars . unVM
+instance (Functor f, Foldable f) => Substitutable f (BoundEnv f) where
+  subReplace w@(BoundEnv s) (BoundEnv t) = BoundEnv (Map.union s (fmap (subReplace w) t))
+  subBoundVars = foldMap subBoundVars . unBoundEnv
 
-newVarM :: M e f VarId
+newVarM :: M e f (Part f)
 newVarM = state $ \st ->
-  let fresh = stUniq st
-      st' = st { stUniq = succ fresh }
-  in (fresh, st')
+  let fresh = stUnique st
+      st' = st { stUnique = succ fresh }
+  in (Part (FreePure fresh), st')
 
--- replaceM :: Part f -> M e f (Part f)
--- replaceM v = do
---   m <- gets stTyEnv
---   pure (subReplace m v)
+replaceM :: (Functor f, Foldable f) => Part f -> M e f (Part f)
+replaceM v = do
+  m <- gets stBoundEnv
+  pure (subReplace m v)
 
 -- valUnifyM :: Val -> Val -> M Val
 -- valUnifyM = go where
@@ -150,39 +190,39 @@ newVarM = state $ \st ->
 --           ValConst -> pure v
 --           _ -> pure (ValErr "fail")
 
--- withEnvM :: (Env f -> Env f) -> M f a -> M f a
--- withEnvM f act = do
---   env <- gets stEnv
---   modify' (\st -> st { stEnv = f env })
---   ret <- act
---   modify' (\st -> st { stEnv = env})
---   pure ret
+withFreeEnvM :: (FreeEnv f -> FreeEnv f) -> M e f a -> M e f a
+withFreeEnvM f act = do
+  freeEnv <- gets stFreeEnv
+  modify' (\st -> st { stFreeEnv = f freeEnv })
+  ret <- act
+  modify' (\st -> st { stFreeEnv = freeEnv})
+  pure ret
 
--- expM :: Exp -> M TyF (Part TyF)
+-- expM :: Exp -> M Void TyF (Part TyF)
 -- expM = \case
---   ExpConst -> pure ValConst
+--   ExpConst -> pure (Part (FreeEmbed TyConstF))
 --   ExpUseBind n -> do
---     Env env <- gets stEnv
+--     FreeEnv env <- gets stFreeEnv
 --     case Map.lookup n env of
---       Just v -> replaceM v
---       Nothing -> pure (ValErr ("not found: " ++ show n))
---   ExpDefBind n x y -> do
---     zx <- expM x
---     withEnvM (\(Env env) -> Env (Map.insert n zx env)) (expM y)
---   ExpTuple x y -> do
---     zx <- expM x
---     zy <- expM y
---     zx' <- replaceM zx
---     pure (ValPair zx' zy)
---   ExpFirst x -> do
---     zx <- expM x
---     v <- newVarM
---     w <- newVarM
---     valUnifyM (ValPair v w) zx
---     replaceM v
---   ExpSecond x -> do
---     zx <- expM x
---     v <- newVarM
---     w <- newVarM
---     valUnifyM (ValPair v w) zx
---     replaceM w
+--       Just v -> pure (Part (FreePure v))
+--       Nothing -> throwError (ErrorMissingFree n)
+  -- ExpDefBind n x y -> do
+  --   zx <- expM x
+  --   withFreeEnvM (\(FreeEnv env) -> FreeEnv (Map.insert n zx env)) (expM y)
+  -- ExpTuple x y -> do
+  --   zx <- expM x
+  --   zy <- expM y
+  --   zx' <- replaceM zx
+  --   pure (ValPair zx' zy)
+  -- ExpFirst x -> do
+  --   zx <- expM x
+  --   v <- newVarM
+  --   w <- newVarM
+  --   valUnifyM (ValPair v w) zx
+  --   replaceM v
+  -- ExpSecond x -> do
+  --   zx <- expM x
+  --   v <- newVarM
+  --   w <- newVarM
+  --   valUnifyM (ValPair v w) zx
+  --   replaceM w
